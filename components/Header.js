a031cab7c5615d8ea6f74d26bcd3cf4aa3773653
JP Given <jgiven@JPs-MacBook-Pro.local>
2020-07-20 09:45:52 -0700

    TIL styled components are slooooooow.

--- components/Header.js
+++ components/Header.js
@@ -1,43 +1,42 @@
-import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
 
-const Head = styled.header`
-  padding: 50px;
-  margin: auto;
-  text-align: center;
-
-  svg {
-    width: 50px !important;
-    height: 50px;
-  }
-`
-
-const Title = styled.h1`
-  font-size: 24px;
-  margin: 20px 0;
-  color: #222;
-`
-
-const Comment = styled.div`
-  margin: auto;
-  max-width: 500px;
-  text-align: left;
-  line-height: 120%;
-  font-size: 14px;
-`
-
 const Header = () => {
   return (
-    <Head>
-      <div style={{ width: '50px', height: '50px', overflow: 'hidden', margin: 'auto' }}>
-        <FontAwesomeIcon icon={faLaptopCode} />
-      </div>
-      <Title>John Patrick Given</Title>
-      <Comment>
-        Is a software developer working for Zappos.com in Las Vegas, Nevada. Below you'll find a random smattering of my online activity.
-      </Comment>
-    </Head>
+    <>
+      <header>
+        <div style={{ width: '50px', height: '50px', overflow: 'hidden', margin: 'auto' }}>
+          <FontAwesomeIcon icon={faLaptopCode} />
+        </div>
+        <h1>John Patrick Given</h1>
+        <div className="comment">
+          Is a software developer working for Zappos.com in Las Vegas, Nevada. Below you'll find a random smattering of my online activity.
+        </div>
+      </header>
+      <style jsx global>{`
+        header {
+          padding: 50px;
+          margin: auto;
+          text-align: center;
+        }
+        header svg {
+          width: 50px !important;
+          height: 50px !important;
+        }
+        h1 {
+          font-size: 24px;
+          margin: 20px 0;
+          color: #222;
+        }
+        .comment {
+          margin: auto;
+          max-width: 500px;
+          text-align: left;
+          line-height: 120%;
+          font-size: 14px;
+        }
+      `}</style>
+    </>
   );
 }
 
